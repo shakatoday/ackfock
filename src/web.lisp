dee5296b9f7c55434ea78052c814549aa30e8475
Shaka Chen <scchen@shaka.today>
2022-03-31 18:44:36 +0800

    done redirect for both when-authorize and unless-authorize

--- src/web.lisp
+++ src/web.lisp
@@ -18,60 +18,70 @@
 ;; Utilities
 (defvar *email-validator* (make-instance 'clavier:email-validator))
 
-(defmacro with-login-user (&body body)
-  "Continue only if the user is logined, redirect to login page otherwise."
+(defmacro when-authorize (&body body)
+  "Continue only if current user is logged-in, redirect to login page otherwise."
   `(cond ((null (ackfock.utils:current-user)) (redirect "/login"))
-           (t ,@body)))
+         (t ,@body)))
+
+(defmacro unless-authorize (&body body)
+  "Continue only if current user is not logged-in, redirect to \"/\" otherwise."
+  `(cond ((ackfock.utils:current-user) (redirect "/"))
+         (t ,@body)))
+
 ;;
 ;; Routing rules
 
 (defroute "/" ()
-  (with-login-user
+  (when-authorize
     (apply #'home-page
            (when (gethash :home-page-message *session*)
              (print (list (pop (gethash :home-page-message *session*))))))))
 
 (defroute "/login" ()
-  (login-page))
+  (unless-authorize
+    (login-page)))
 
 (defroute ("/login" :method :POST) (&key _parsed)
-  (let ((email (cdr (assoc "email" _parsed :test #'string=)))
-        (password (cdr (assoc "password" _parsed :test #'string=))))
-    (if (setf (ackfock.utils:current-user) (ackfock.model:authenticate email password))
-        (redirect "/")
-        (login-page :message "Email or password incorrect"))))
+  (unless-authorize
+    (let ((email (cdr (assoc "email" _parsed :test #'string=)))
+          (password (cdr (assoc "password" _parsed :test #'string=))))
+      (if (setf (ackfock.utils:current-user) (ackfock.model:authenticate email password))
+          (redirect "/")
+          (login-page :message "Email or password incorrect")))))
 
 (defroute ("/logout" :method :POST) ()
   (setf (ackfock.utils:current-user) nil)
   (redirect "/"))
 
 (defroute "/sign-up" ()
-  (login-page :sign-up t))
+  (unless-authorize
+    (login-page :sign-up t)))
 
 (defroute ("/sign-up" :method :POST) (&key _parsed)
-  (let ((email (cdr (assoc "email" _parsed :test #'string=)))
-        (username (cdr (assoc "username" _parsed :test #'string=)))
-        (password (cdr (assoc "password" _parsed :test #'string=))))
-    (cond ((find t
-                 (list email username password)
-                 :key #'str:emptyp) (login-page :message "Email, username, or password empty"
-                                                :sign-up t))
-          ((null (clavier:validate *email-validator*
-                                   email)) (login-page :message "Not a valid email address"
-                                                       :sign-up t))
-          (t (setf (ackfock.utils:current-user)
-                   (ackfock.model:new-user email username password))
-             (redirect "/")))))
+  (unless-authorize
+    (let ((email (cdr (assoc "email" _parsed :test #'string=)))
+          (username (cdr (assoc "username" _parsed :test #'string=)))
+          (password (cdr (assoc "password" _parsed :test #'string=))))
+      (cond ((find t
+                   (list email username password)
+                   :key #'str:emptyp) (login-page :message "Email, username, or password empty"
+                                                  :sign-up t))
+            ((null (clavier:validate *email-validator*
+                                     email)) (login-page :message "Not a valid email address"
+                                                         :sign-up t))
+            (t (setf (ackfock.utils:current-user)
+                     (ackfock.model:new-user email username password))
+               (redirect "/"))))))
 
 (defroute ("/add-memo" :method :POST) (&key _parsed)
-  (with-login-user
+  (when-authorize
     (let ((content (cdr (assoc "content" _parsed :test #'string=))))
       (unless (str:emptyp content)
         (ackfock.model:new-memo (ackfock.utils:current-user) content))))
   (redirect "/"))
 
 (defroute ("/ackfock-memo" :method :POST) (&key _parsed)
-  (with-login-user
+  (when-authorize
     (let ((memo-uuid (cdr (assoc "uuid" _parsed :test #'string=)))
           (ackfock (cdr (assoc "ackfock" _parsed :test #'string=)))
           (as-target-user-ackfock (and (assoc "as_target_user_ackfock" _parsed :test #'string=)
@@ -84,7 +94,7 @@
 
 ;; TODO: solve race condition
 (defroute ("/send-memo" :method :POST) (&key _parsed)
-  (with-login-user
+  (when-authorize
     (let ((memo-uuid (cdr (assoc "uuid" _parsed :test #'string=)))
           (recipient (ackfock.model:get-user-by-email (cdr (assoc "recipient" _parsed :test #'string=)))))
       (if recipient
