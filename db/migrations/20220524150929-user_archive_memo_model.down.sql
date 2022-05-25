85cef0b3c794565eedfc61c0829d61145b0101aa
Shaka Chen <scchen@shaka.today>
2022-05-25 16:49:43 +0800

    done user_archive_memo_model migration sql

--- db/migrations/20220524150929-user_archive_memo_model.down.sql
+++ db/migrations/20220524150929-user_archive_memo_model.down.sql
@@ -1,5 +1,11 @@
 -- id: 20220524150929
 -- direction: DOWN
 -- description: user_archive_memo_model
-
-
+DROP TABLE "public"."user_ackfock";
+--;;
+ALTER TABLE "public"."memo"
+  DROP COLUMN archive_id;
+--;;
+DROP TABLE "public"."user_archive_access";
+--;;
+DROP TABLE "public"."archive";
