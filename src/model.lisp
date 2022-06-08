12b79b4dd44d2c9f2742452c88631c01ad9e7e69
Shaka Chen <scchen@shaka.today>
2022-06-09 03:00:42 +0800

    dummy functions for new model-definitions in model.lisp

--- src/model.lisp
+++ src/model.lisp
@@ -50,46 +50,13 @@
                :test #'string=)
        (alexandria:make-keyword string)))
 
-(defun-with-db-connection new-user (email username password)
-  "Insert a new user into database and return an ACKFOCK.MODEL::USER instance"
-  (retrieve-one 
-   (insert-into :users
-     $(set= email
-            username
-            :password_salted (cl-pass:hash password))
-     (returning :*))
-   :as 'user))
+(defun-with-db-connection new-memo (user archive content))
 
-(defun-with-db-connection user-memos (user &key (as-source-user t) as-target-user (limit 20) (offset 0))
-  (retrieve-all
-   (select :*
-     (from :memo)
-     (where (:or (:= :source_user_id (cond ((null as-source-user) (dummy-uuid)) ; for uuid type consistency in postgresql
-                                           (t (user-uuid user))))
-                 (:= :target_user_id (cond ((null as-target-user) (dummy-uuid)) ; for uuid type consistency in postgresql
-                                           (t (user-uuid user))))))
-     (order-by (:desc :updated_at))
-     (limit limit)
-     (offset offset))
-   :as 'memo))
+(defun-with-db-connection new-archive (user))
 
-(defun-with-db-connection authenticate (email password)
-  (let ((user-data (retrieve-one
-                    (select :*
-                      (from :users)
-                      (where (:= :email email))))))
-    (and user-data
-         (cl-pass:check-password password
-                                 (getf user-data :password-salted))
-         (apply #'make-user
-                user-data))))
+(defun-with-db-connection invite-to-archive (source-user target-user))
 
-(defun-with-db-connection new-memo (user content)
-  (when (user-p user)
-    (execute
-     (insert-into :memo
-       $(set= :source_user_id (user-uuid user)
-              content)))))
+(defun-with-db-connection memo-user-ackfocks (memo))
 
 (defun-with-db-connection get-user-by-email (email)
   (retrieve-one
