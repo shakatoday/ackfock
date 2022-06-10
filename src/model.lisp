c72db699fd90c2915cc8718110baad390adb825a
Shaka Chen <scchen@shaka.today>
2022-06-10 21:42:26 +0800

    ackfock.model:new-memo and some dummy functions

--- src/model.lisp
+++ src/model.lisp
@@ -27,17 +27,41 @@
                :test #'string=)
        (alexandria:make-keyword string)))
 
-(defun-with-db-connection new-memo (user archive content))
+(defun-with-db-connection new-memo (user-token archive-id content)
+  (handler-case
+      ;; race condition notice below!
+      (let* ((user-id (user-uuid (user-by-user-token user-token))))
+        (execute
+         (if (retrieve-one
+                  (select :*
+                    (from :user_archive_access)
+                    (where #.(utils-ackfock:ensure-plist '(:=
+                                                           user-id
+                                                           archive-id)))))
+             (insert-into :memo
+               #.(utils-ackfock:ensure-plist '(:=
+                                               :creator_id user-id
+                                               content
+                                               archive-id)))
+             (insert-into :memo
+               #.(utils-ackfock:ensure-plist '(:=
+                                               :creator_id user-id
+                                               content))))))
+    (type-error () nil)))
 
-(defun-with-db-connection new-archive (user))
+(defun-with-db-connection new-archive (user-token))
 
-(defun-with-db-connection invite-to-archive (source-user target-user))
+(defun-with-db-connection invite-to-archive (source-user-token target-user-email archive-id))
 
-(defun-with-db-connection memo-user-ackfocks (memo))
+(defun-with-db-connection add-memo-to-archive (source-user-token memo-id archive-id))
 
-(defun-with-db-connection get-user-by-email (email)
+(defun-with-db-connection memo-user-ackfocks (user-token memo-id))
+
+(defun-with-db-connection ackfock-memo (user-token memo-id ackfock))
+
+(defun-with-db-connection user-by-user-token (user-token)
   (retrieve-one
    (select :*
      (from :users)
-     (where #.(ackfock.utils:ensure-plist '(:= email))))
+     (where (:= :token user-token)))
    :as 'user))
