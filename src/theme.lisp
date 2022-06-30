40bc8670916b94f3a46e46737a6e9ff41dfddf2c
Shaka Chen <scchen@shaka.today>
2022-07-01 00:28:22 +0800

    bind search input with the last time input

--- src/theme.lisp
+++ src/theme.lisp
@@ -46,7 +46,7 @@ Page properties:
           (div (:bind menu-inner-div)
                (form (:action "/search" :method :POST)
                      (a (:link (url website) :content (title website) :class "w3-xlarge w3-sans-serif w3-margin clog-theme"))
-	             (form-element (:search :name "search" :class "w3-margin-left" :hidden (not (profile website))))
+	             (form-element (:bind search-input :search :name "search" :class "w3-margin-left" :hidden (not (profile website))))
                      (button (:content "search" :hidden (not (profile website)))))
                (div (:bind menu-right-div)))
         (center-children menu-inner-div
@@ -62,45 +62,50 @@ Page properties:
                                                    :link "/logout"))
 	    (when login-link
 	      (create-web-menu-item menu-right-div :content "login"
-					           :link login-link))))
-      (add-class menu color-class)
-      (setf (height menu-margin-div) (height menu)))
-    ;; SECTION: Content area
-    (when content
-      (typecase content
-	(string
-	 (create-div body :content content))
-	(function
-	 (funcall content body))
-	(t
-	 (create-div body :content (format nil "~A" content)))))
-    ;; SECTION: Special pages - Login
-    (when (eq page :login)
-      (let* ((outter    (create-web-container body))
-	     (form      (create-form outter))
-	     (p1        (create-p form))
-	     (l1        (create-label p1 :content "Email"
-					 :class text-class))
-	     (user      (create-form-element p1 :email
-					     :name "email"
-					     :class (format nil "w3-input ~A" border-class)))
-	     (p2        (create-p form))
-	     (l2        (create-label p2 :content "Password"
-					 :class text-class))
-	     (pass      (create-form-element p2 :password
-					     :name "password"
-					     :class (format nil "w3-input ~A" border-class)))
-	     (p3        (create-p form)))
-	(declare (ignore l1 l2 p3))
-	(setf (maximum-width outter) (unit :px 500))
-	(setf (requiredp user) t)
-	(setf (requiredp pass) t)
-	(create-form-element form :submit :value "Submit"
-					  :class (format nil "~A ~A" "w3-button" color-class))
-	(set-on-submit form (getf properties :on-submit))
-	(when signup-link
-	  (create-a form :class "w3-right" :content "sign up" :link signup-link))))
-    ;; SECTION: Footer
-    (create-br body)
-    (create-br body)
-    (create-div body :content (format nil "~A" (footer website)))))
+					           :link login-link)))
+        (add-class menu color-class)
+        (setf (height menu-margin-div) (height menu))
+        ;; SECTION: Content area
+        (when content
+          (typecase content
+	    (string
+	     (create-div body :content content))
+	    (function
+	     (funcall content body))
+	    (t
+	     (create-div body :content (format nil "~A" content)))))
+        ;; SECTION: Special pages - Login
+        (cond ((eq page :login)
+               (let* ((outter    (create-web-container body))
+	              (form      (create-form outter))
+	              (p1        (create-p form))
+	              (l1        (create-label p1 :content "Email"
+					          :class text-class))
+	              (user      (create-form-element p1 :email
+					              :name "email"
+					              :class (format nil "w3-input ~A" border-class)))
+	              (p2        (create-p form))
+	              (l2        (create-label p2 :content "Password"
+					          :class text-class))
+	              (pass      (create-form-element p2 :password
+					              :name "password"
+					              :class (format nil "w3-input ~A" border-class)))
+	              (p3        (create-p form)))
+	         (declare (ignore l1 l2 p3))
+	         (setf (maximum-width outter) (unit :px 500))
+	         (setf (requiredp user) t)
+	         (setf (requiredp pass) t)
+	         (create-form-element form :submit :value "Submit"
+					           :class (format nil "~A ~A" "w3-button" color-class))
+	         (set-on-submit form (getf properties :on-submit))
+	         (when signup-link
+	           (create-a form :class "w3-right" :content "sign up" :link signup-link))))
+              ((eq page :search)
+               (rutils:when-it (form-data-item (form-post-data body)
+                                               "search")
+                 (setf (text-value search-input) rutils:it)))))
+
+      ;; SECTION: Footer
+      (create-br body)
+      (create-br body)
+      (create-div body :content (format nil "~A" (footer website))))))
