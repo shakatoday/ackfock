d9469b842b61e3be5be5d692f372beda7b10ce02
Shaka Chen <scchen@shaka.today>
2022-08-20 01:14:18 +0800

    removed :use :clog :clog-web from ackfock.lisp

--- src/ackfock.lisp
+++ src/ackfock.lisp
@@ -1,7 +1,8 @@
 (in-package :cl-user)
 (defpackage #:ackfock
-  (:use #:cl #:clog #:clog-web)
-  (:export #:start-app))
+  (:use #:cl)
+  (:export #:start-app
+           #:stop))
 (in-package :ackfock)
 
 (defparameter *landing-page*
@@ -12,27 +13,31 @@
 
 (defun start-app (&key (port 8080) (open-browser-p nil))
   ;; Setup clog
-  (initialize nil
-              :port port
-              :lack-middleware-list `(,lack.middleware.session:*lack-middleware-session*
-                                      ,clog-lack-session:*middleware*
-                                      ,(lambda (app)
-                                         (lambda (env)
-                                           (cond ((and (string= (getf env
-                                                                      :path-info)
-                                                                "/")
-                                                       (null (gethash :current-user
-                                                                      (getf env
-                                                                            :lack.session))))
-                                                  `(200 (:content-type "text/html")
-                                                        (,*landing-page*)))
-                                                 (t
-                                                  (funcall app env))))))
-	      :extended-routing t
-              :static-root (merge-pathnames "./www/"
-	                                    (asdf:system-source-directory :ackfock))
-	      :boot-function (clog-web-meta
-			      "Ackfock is a platform of mini agreements and mini memos of understanding."))
+  (clog:initialize nil
+                   :port port
+                   :lack-middleware-list `(,lack.middleware.session:*lack-middleware-session*
+                                           ,clog-lack-session:*middleware*
+                                           ,(lambda (app)
+                                              (lambda (env)
+                                                (cond ((and (string= (getf env
+                                                                           :path-info)
+                                                                     "/")
+                                                            (null (gethash :current-user
+                                                                           (getf env
+                                                                                 :lack.session))))
+                                                       `(200 (:content-type "text/html")
+                                                             (,*landing-page*)))
+                                                      (t
+                                                       (funcall app env))))))
+	           :extended-routing t
+                   :static-root (merge-pathnames "./www/"
+	                                         (asdf:system-source-directory :ackfock))
+	           :boot-function (clog-web:clog-web-meta
+			           "Ackfock is a platform of mini agreements and mini memos of understanding."))
   (ackfock.game.pages:define-all)
   (when open-browser-p
-    (open-browser)))
+    (clog:open-browser :url (format nil "http://127.0.0.1:~a"
+                                    port))))
+
+(defun stop ()
+  (clog:shutdown))
