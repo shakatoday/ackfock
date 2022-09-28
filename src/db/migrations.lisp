977b1c3ffaab873d4a8b9e75b3f2a2b3ec30754c
Shaka Chen <scchen@shaka.today>
2022-09-28 17:32:40 +0800

    halfway writing mito auto-pk compat migration

--- src/db/migrations.lisp
+++ src/db/migrations.lisp
@@ -18,3 +18,22 @@
     (mito:execute-sql
      (alter-table :user
        (rename-to :users)))))
+
+(defun id-and-primary-key-compatibility/upgrade (migration-driver)
+  "Alter all tables' uuid column names to id, and alter types to varchar(36)."
+  (let ((mito:*connection* (migratum.driver.dbi:dbi-driver-connection migration-driver)))
+    (mapc (lambda (table-name)
+            (mito:execute-sql
+             (alter-table table-name
+               (rename-column :uuid :id))))
+          '(:user :channel :memo))
+    (mapc (lambda (table-name constraint-name)
+            (mito:execute-sql
+             (format nil "ALTER TABLE ~(~a~) ALTER COLUMN DROP CONSTRAINT ~(~a~)"
+                     table-name
+                     constraint-name)))
+          '(:user_ackfock :user_ackfock
+            '(:user_ackfock_memo_id_fkey :user_ackfock_user_id_fkey
+
+(defun id-and-primary-key-compatibility/downgrade (migration-driver)
+  "Alter all tables' id column names back to uuid, and alter the types back to uuid")
