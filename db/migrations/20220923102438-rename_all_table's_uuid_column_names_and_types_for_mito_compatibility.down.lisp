c77037153a491252707266365cd48dd305aa1609
Shaka Chen <scchen@shaka.today>
2022-09-29 16:17:24 +0800

    migration spec

---
+++ db/migrations/20220923102438-rename_all_table's_uuid_column_names_and_types_for_mito_compatibility.down.lisp
@@ -0,0 +1,6 @@
+;; id: 20220923102438
+;; direction: DOWN
+;; description: rename_all_table's_uuid_column_names_and_types_for_mito_compatibility
+
+(:system ackfock-db-migrations :package ackfock.db.migrations :handler
+ id-and-primary-key-compatibility/downgrade)
---
+++ db/migrations/20220923102438-rename_all_table's_uuid_column_names_and_types_for_mito_compatibility.up.lisp
@@ -0,0 +1,6 @@
+;; id: 20220923102438
+;; direction: UP
+;; description: rename_all_table's_uuid_column_names_and_types_for_mito_compatibility
+
+(:system ackfock-db-migrations :package ackfock.db.migrations :handler
+ id-and-primary-key-compatibility/upgrade)
