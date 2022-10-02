06c9bb080f9e79bdb2e5842912d153fccb66bb3c
Shaka Chen <scchen@shaka.today>
2022-10-03 07:01:40 +0800

    use macro. use :account rather than :user

--- src/db/migrations.lisp
+++ src/db/migrations.lisp
@@ -21,76 +21,73 @@
        (let ((mito:*connection* (migratum.driver.dbi:dbi-driver-connection migration-driver)))
          ,@body))))
 
-(defun rename-user-table-for-using-mito/upgrade (migration-driver)
+(define-migration-handler rename-user-table-for-using-mito/upgrade
   "To change users to user. And ensuring it's letter case is compatible"
-  (let ((mito:*connection* (migratum.driver.dbi:dbi-driver-connection migration-driver)))
-    (mito:execute-sql
-     (alter-table :users
-       (rename-to :account)))))
+  (mito:execute-sql
+   (alter-table :users
+     (rename-to :account))))
 
-(defun rename-user-table-for-using-mito/downgrade (migration-driver)
+(define-migration-handler rename-user-table-for-using-mito/downgrade
   "To change table user back to users."
-  (let ((mito:*connection* (migratum.driver.dbi:dbi-driver-connection migration-driver)))
-    (mito:execute-sql
-     (alter-table :account
-       (rename-to :users)))))
+  (mito:execute-sql
+   (alter-table :account
+     (rename-to :users))))
 
 (define-migration-handler rename-all-db-object-names-from-user-to-account/upgrade)
 
 (define-migration-handler rename-all-db-object-names-from-user-to-account/downgrade)
 
-(defun id-and-primary-key-compatibility/upgrade (migration-driver)
+(define-migration-handler id-and-primary-key-compatibility/upgrade
   "Alter all tables' uuid column names to id, and alter types to varchar(36)."
-  (let ((mito:*connection* (migratum.driver.dbi:dbi-driver-connection migration-driver)))
-    ;;
-    ;; rename uuid column to id
-    (mapc (lambda (table-name)
+  ;;
+  ;; rename uuid column to id
+  (mapc (lambda (table-name)
+          (mito:execute-sql
+           (alter-table table-name
+             (rename-column :uuid :id))))
+        '(:account :channel :memo))
+  ;;
+  ;; drop foriegn keys
+  (mapc (lambda (table-name constraint-table-name reference-column-name)
+          (mito:execute-sql
+           (format nil "ALTER TABLE ~(~a~) DROP CONSTRAINT ~(~a~)_~(~a~)_fkey"
+                   table-name
+                   (or constraint-table-name
+                       table-name)
+                   reference-column-name)))
+        *foreign-key-tables*
+        '(nil nil nil nil nil :user_archive_access :user_archive_access nil nil nil)
+        *foreign-key-columns*)
+  ;;
+  ;; change :uuid type to varchar(36)
+  (mapc (lambda (table-name column-name)
+          (mito:execute-sql
+           (alter-table table-name
+             (alter-column column-name :type '(:varchar 36)))))
+        (append '(:account :channel :memo)
+                *foreign-key-tables*)
+        (append '(:id :id :id)
+                (substitute :channel_id
+                            :archive_id
+                            *foreign-key-columns*)))
+  ;;
+  ;; re-construct foreign key constraints
+  (mapc (lambda (table-name column-name)
+          (let* ((column-name (if (eq column-name :archive_id)
+                                  :channel_id
+                                  column-name))
+                 (reference-table-name (or (case column-name
+                                             ((:memo_id :parent_memo_id) :memo)
+                                             (:channel_id :channel)
+                                             ((:user_id :creator_id :source_user_id :used_by_user_id) :account))
+                                           (error "Column: ~(~a~) doesn't have corresponding reference-table-name" column-name))))
             (mito:execute-sql
-             (alter-table table-name
-               (rename-column :uuid :id))))
-          '(:user :channel :memo))
-    ;;
-    ;; drop foriegn keys
-    (mapc (lambda (table-name constraint-table-name reference-column-name)
-            (mito:execute-sql
-             (format nil "ALTER TABLE ~(~a~) DROP CONSTRAINT ~(~a~)_~(~a~)_fkey"
+             (format nil "ALTER TABLE ~(~a~) ADD ~a"
                      table-name
-                     (or constraint-table-name
-                         table-name)
-                     reference-column-name)))
-          *foreign-key-tables*
-          '(nil nil nil nil nil :user_archive_access :user_archive_access nil nil nil)
-          *foreign-key-columns*)
-    ;;
-    ;; change :uuid type to varchar(36)
-    (mapc (lambda (table-name column-name)
-            (mito:execute-sql
-             (alter-table table-name
-               (alter-column column-name :type '(:varchar 36)))))
-          (append '(:user :channel :memo)
-                  *foreign-key-tables*)
-          (append '(:id :id :id)
-                  (substitute :channel_id
-                              :archive_id
-                              *foreign-key-columns*)))
-    ;;
-    ;; re-construct foreign key constraints
-    (mapc (lambda (table-name column-name)
-            (let* ((column-name (if (eq column-name :archive_id)
-                                    :channel_id
-                                    column-name))
-                   (reference-table-name (or (case column-name
-                                              ((:memo_id :parent_memo_id) :memo)
-                                              (:channel_id :channel)
-                                              ((:user_id :creator_id :source_user_id :used_by_user_id) :user))
-                                            (error "Column: ~(~a~) doesn't have corresponding reference-table-name" column-name))))
-              (mito:execute-sql
-               (format nil "ALTER TABLE ~(~a~) ADD ~a"
-                       table-name
-                       (yield
-                        (foreign-key `(,column-name) :references `(,reference-table-name :id)))))))
-          *foreign-key-tables*
-          *foreign-key-columns*)))
+                     (yield
+                      (foreign-key `(,column-name) :references `(,reference-table-name :id)))))))
+        *foreign-key-tables*
+        *foreign-key-columns*))
 
-(defun id-and-primary-key-compatibility/downgrade (migration-driver)
+(define-migration-handler id-and-primary-key-compatibility/downgrade
   "Alter all tables' id column names back to uuid, and alter the types back to uuid")
